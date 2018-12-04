3bdaad3fb696d7a5f5066009a72d50b1fc7c9736
Mary Matthews <matthewsmp@gmail.com>
2018-12-04 12:25:43 -0800

    fix date on confirmation form and update registration styling

--- client/src/components/Confirmation.jsx
+++ client/src/components/Confirmation.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 function Confirmation(props) {
   return (
     <div>
-      CONFIRMATION: Thanks for registering for {props.eventData.title} on {props.eventData.date}.
+      CONFIRMATION: Thanks for registering for {props.eventData.title} on {(props.eventData.date).slice(0, 10)}.
       <button>See who else is coming.</button>
       <button onClick={props.updateToGroupsView}>Find my group!</button>
     </div>
--- client/src/components/RegistrationForm.jsx
+++ client/src/components/RegistrationForm.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import $ from 'jquery';
+// const styled = require('styled-components');
 
-// receieves from props updateEventData to update the state after post
+// const FormWrapper = styled.form`
+//   display: flex;
+//   flex-direction: column;
+// `;
 
 class RegistrationForm extends React.Component {
 
@@ -18,16 +22,12 @@ class RegistrationForm extends React.Component {
       email: '',
       photoURL: '',
     };
-
     this.handleChange = this.handleChange.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-
   }
 
   handleSubmit(event) {
-    // post to database
     event.preventDefault();
-
     $.ajax({
       type: 'POST',
       url: '/api/attendees',
@@ -69,59 +69,77 @@ class RegistrationForm extends React.Component {
     return (
       <div>
         <h2>Fill out the form here to register for {this.props.eventData.title}.</h2>
+        {/* <FormWrapper onSubmit={this.handleSubmit}> */}
         <form onSubmit={this.handleSubmit}>
-          <label>
-            First Name:
-            <input type="text" name='firstName' value={this.state.firstName} onChange={this.handleChange} />
-          </label>
+          <div>
+            <label>
+              First Name:
+              <input type="text" name='firstName' value={this.state.firstName} onChange={this.handleChange} />
+            </label>
+          </div>
 
-          <label>
-            Last Name:
-          <input type="text" name='lastName' value={this.state.lastName} onChange={this.handleChange} />
-          </label>
+          <div>
+            <label>
+              Last Name:
+              <input type="text" name='lastName' value={this.state.lastName} onChange={this.handleChange} />
+            </label>
+          </div>
 
-          <label>
-            Company:
-          <input type="text" name='company' value={this.state.company} onChange={this.handleChange} />
-          </label>
+          <div>
+            <label>
+              Company:
+            <input type="text" name='company' value={this.state.company} onChange={this.handleChange} />
+            </label>
+          </div>
 
-          <label>
-            Select the industry that best represents your work
-          <select value={this.state.industry} name='industry' onChange={this.handleChange}>
-              <option value="tech">Tech</option>
-              <option value="healthcare">Healthcare</option>
-              <option value="nonprofit">Nonprofit</option>
-              <option value="law">Law</option>
-              <option value="finance">Finance</option>
-              <option value="marketing">Marketing</option>
-              <option value="other">Other</option>
-            </select>
-          </label>
+          <div>
+            <label>
+              Select the industry that best represents your work
+            <select value={this.state.industry} name='industry' onChange={this.handleChange}>
+                <option value="tech">Tech</option>
+                <option value="healthcare">Healthcare</option>
+                <option value="nonprofit">Nonprofit</option>
+                <option value="law">Law</option>
+                <option value="finance">Finance</option>
+                <option value="marketing">Marketing</option>
+                <option value="other">Other</option>
+              </select>
+            </label>
+          </div>
 
-          <label>
-            How long have you lived in {this.props.eventData.city} ?
-          <select value={this.state.local} name='local' onChange={this.handleChange}>
-              <option value={true}>3 years or less</option>
-              <option value={false}>more than 3 years</option>
-            </select>
-          </label>
+          <div>
+            <label>
+              How long have you lived in {this.props.eventData.city} ?
+            <select value={this.state.local} name='local' onChange={this.handleChange}>
+                <option value={true}>3 years or less</option>
+                <option value={false}>more than 3 years</option>
+              </select>
+            </label>
+          </div>
 
-          <label>
-            LinkedInURL:
-          <input type="text" name='linkedInURL' value={this.state.linkedIn} onChange={this.handleChange} />
-          </label>
+          <div>
+            <label>
+              LinkedInURL:
+              <input type="text" name='linkedInURL' value={this.state.linkedIn} onChange={this.handleChange} />
+            </label>
+          </div>
 
-          <label>
-            Email:
-          <input type="text" name='email' value={this.state.email} onChange={this.handleChange} />
-          </label>
+          <div>
+            <label>
+              Email:
+              <input type="text" name='email' value={this.state.email} onChange={this.handleChange} />
+            </label>
+          </div>
 
-          <label>
-            PhotoURL:
-          <input type="text" name='photoURL' value={this.state.photo} onChange={this.handleChange} />
-          </label>
+          <div>
+            <label>
+              PhotoURL:
+              <input type="text" name='photoURL' value={this.state.photo} onChange={this.handleChange} />
+            </label>
+          </div>
 
           <input type="submit" value="Submit" />
+          {/* </FormWrapper> */}
         </form>
       </div >
     );
